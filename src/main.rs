2a02920ffed1c8de1b7eb190bee1af1cfa9974cd
4prndz <victorkouichi40@gmail.com>
2024-03-08 01:30:04 -0300

    fix: add underscore in unused parameters

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ fn get_posts() -> Value {
     json!([{"id": 1, "name": "4prndz"}, {"id": 2, "name": "Kouichi"}])
 }
 
-#[get("/posts/<id>")]
-fn view_post(id: u32) -> Value {
+#[get("/posts/<_id>")]
+fn view_post(_id: u32) -> Value {
     json!({"id": 1, "name": "4prndz", "title": "Quarter of Silence"})
 }
 
@@ -21,13 +21,13 @@ fn create_post() -> Value {
     json!({"id": 1, "name": "4prndz", "title": "Quarter of Silence"})
 }
 
-#[put("/posts/<id>", format = "json")]
-fn update_post(id: u32) -> Value {
+#[put("/posts/<_id>", format = "json")]
+fn update_post(_id: u32) -> Value {
     json!({"id": 1, "name": "4prndz", "title": "Quarter of Silence"})
 }
 
-#[delete("/posts/<id>")]
-fn delete_post(id: u32) -> status::NoContent {
+#[delete("/posts/<_id>")]
+fn delete_post(_id: u32) -> status::NoContent {
     status::NoContent
 }
 
